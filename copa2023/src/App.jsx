06c426c5ec75cc736906388b4cc6a98a2401835e
Deividson Henry <deividson.henrique2016@hotmail.com>
2024-05-23 20:45:21 -0300

    day 23.05.24

---
+++ copa2023/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Card from './components/Card'
+import GameTable from './components/GameTable'
+
+function App() {
+
+  return (
+    <>
+      <h1>Copa do Mundo Femenina 2023</h1>
+      <section className='cards'>
+        <Card />
+      </section>
+
+      <h2>Tabela de Jogos</h2>
+
+      <section className='game_table'>
+        <GameTable />
+      </section>
+
+    </>
+  )
+}
+
+export default App
