24ffe7a2466f579f3eef71152c2fff50801d6837
Deividson Henry <109252541+DeividsonHenrique@users.noreply.github.com>
2024-10-15 22:23:04 -0300

    day 15.10.24

--- api-rest/src/app.js
+++ api-rest/src/app.js
@@ -1,38 +1,86 @@
 import express from 'express';
 
+import conexao from '../infra/conexao.js';
+
 const app = express();
 
-//mock
+app.use(express.json());
 
-const selecoes = [
-    {
-        id: 1,
-        nome: 'Brasil',
-        grupo: 'G'
-    },
-    {
-        id: 2,
-        nome: 'Suiça',
-        grupo: 'G'
-    },
-    {
-        id: 3,
-        nome: 'Servia',
-        grupo: 'G'
-    },
-    {
-        id: 4,
-        nome: 'Camaroes',
-        grupo: 'G'
-    }
-]
 
-app.get('/', (req, res) => {
-    res.send('Curso de API REST com NodeJS');
-})
+const buscarSelecao = (id) => {
+    return selecoes.filter(selecao => selecao.id == id);
+}
+
+const BuscarIndexSelecao = (id) => {
+    return selecoes.findIndex(selecao => selecao.id == id);
+}
 
 app.get('/selecoes', (req, res) => {
-    res.status(200).send(selecoes);
+    // res.status(200).send(selecoes);
+    const sql = 'SELECT * FROM selecoes';
+    conexao.query(sql, (err, result) => {
+        if (err) {
+            res.status(404).json({error: 'erro ao buscar seleções'});
+        }
+        res.status(200).json(result);
+    })
+})
+
+app.get('/selecoes/:id', (req, res) => {
+    // res.json(buscarSelecao(req.params.id));
+    const id = req.params.id;
+    const sql = 'SELECT * FROM selecoes WHERE id = ?';
+    conexao.query(sql, id, (err, result) => {
+        const linha = result[0];
+        if (err) {
+            res.status(404).json({error: 'erro ao buscar seleções'});
+        }
+        res.status(200).json(linha);
+    })
+})
+
+app.post('/selecoes', (req, res) => {
+    // selecoes.push(req.body);
+    // res.status(201).send("seleção criada com sucesso");
+    const selecao = req.body;
+    const sql = 'INSERT INTO selecoes SET ?';
+    conexao.query(sql, selecao, (err, result) => {
+        if (err) {
+            res.status(400).json({error: 'erro ao buscar seleções'});
+        }
+        res.status(201).json(result);
+    })
+
+})
+
+app.delete('/selecoes/:id', (req, res) => {
+    // const index = BuscarIndexSelecao(req.params.id);
+    // selecoes.splice(index, 1);
+    // res.status(200).send(`seleção com id:${req.params.id} deletada com sucesso`);
+    const id = req.params.id;
+    const sql = 'DELETE FROM selecoes WHERE id = ?';
+    conexao.query(sql, id, (err, result) => {
+        if (err) {
+            res.status(404).json({error: 'erro ao buscar seleções'});
+        }
+        res.status(200).json(result);
+    })
+})
+
+app.put('/selecoes/:id', (req, res) => {
+    // const index = BuscarIndexSelecao(req.params.id);
+    // selecoes[index].nome = req.body.nome;
+    // selecoes[index].grupo = req.body.grupo;
+    // res.json(selecoes);
+    const id = req.params.id;
+    const selecao = req.body;
+    const sql = 'UPDATE selecoes SET ? WHERE id = ?';
+    conexao.query(sql, [selecao, id], (err, result) => {
+        if (err) {
+            res.status(400).json({error: 'erro ao buscar seleções'});
+        }
+        res.status(200).json(result);
+    })
 })
 
 export default app
